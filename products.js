1185fd112de5163245ecbd11dfb595b30b29ced7
priyankasubu <priyankasubu95@gmail.com>
2025-10-09 12:45:02 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,11 +1,3 @@
 const products = [
-  // Example product (will be auto-added by n8n)
-  // {
-  //   title: "Sample Product",
-  //   price: "₹999",
-  //   image: "https://via.placeholder.com/250",
-  //   link: "#",
-  //   category: "Mobiles",
-  //   subcategory: ""
-  // }
+  // This array gets automatically updated by n8n workflow
 ];
